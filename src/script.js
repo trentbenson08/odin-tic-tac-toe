1bac809119bc86c4c0714e58a26e84593dc3450b
trent <122744132+trentbenson08@users.noreply.github.com>
2023-04-29 20:22:21 -0500

    Add checkWinCondition

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
-
-const obj = ((doc) => {
+((doc) => {
 	
 
   const boardState = new Array(9).fill(undefined);
@@ -7,10 +6,28 @@ const obj = ((doc) => {
 		{turn: 1},
 		{name: 'Player 1', marker: 'x'},
 		{name: 'Player 2', marker: 'o'}
-	]
+	];
 
-  // cache DOM
+
   const cells = doc.querySelectorAll('.cell');
+	const newGame = doc.querySelector('#new-game');
+
+
+	function checkWinCondition(){
+		const winningPositions = [    
+			[0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    	[0, 3, 6], [1, 4, 7], [2, 5, 8], 
+    	[0, 4, 8], [2, 4, 6]
+  	];
+		winningPositions.forEach((pos) => {
+			if (boardState[pos[0]] === undefined && boardState[pos[1]] === undefined && boardState[pos[2]] === undefined) {
+				return;
+			}
+			if (boardState[pos[0]] === boardState[pos[1]] && boardState[pos[1]] === boardState[pos[2]]) {
+				console.log('WINNER');
+			}
+		});
+	}
 
 
 	function renderBoard(){
@@ -19,13 +36,16 @@ const obj = ((doc) => {
 				cells[i].innerText = 'X';
 			} else if (boardState[i] === 'o') {
 				cells[i].innerText = 'O';
+			} else {
+				cells[i].innerText = '';
 			}
 		}
 	}
 
+
 	function alterBoard(event){
 		const cellIndex = event.target.getAttribute('data-index');
-		if (boardState[cellIndex] !== undefined){
+		if (boardState[cellIndex] !== undefined) {
 			return;
 		}
 		let mark;
@@ -38,12 +58,21 @@ const obj = ((doc) => {
 		}
 		boardState[cellIndex] = mark;
 		renderBoard();
+		checkWinCondition();
 	}
 
 
+	function clearBoard(){
+		for (let i = 0; i < boardState.length; i += 1) {
+			boardState[i] = undefined;
+		}
+		playerState[0].turn = 1;
+		renderBoard();
+	}
 
 
+	cells.forEach((cell) => {cell.addEventListener('click', alterBoard);});
+	newGame.addEventListener('click', clearBoard);
 
-	// event listners
-	cells.forEach((cell) => {cell.addEventListener('click', alterBoard)});
+
 })(document);
